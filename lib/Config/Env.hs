84b6162de20c0198a392f03b7adbb1f26b15a285
Dan Dart <git@dandart.co.uk>
2025-06-05 14:45:51 +0100

    updates

--- lib/Config/Env.hs
+++ lib/Config/Env.hs
@@ -4,11 +4,12 @@ module Config.Env where
 
 import Config.Foreign
 import Control.Exception
-import Data.Text
+import Data.Text qualified as T
+import Data.Text (Text)
 import Language.Haskell.TH
 
 apiHostIO ∷ IO Text
-apiHostIO = pack <$> catch (envVar "API_HOST") (\(SomeException ex) -> pure $ "API Host Runtime Unset" <> show ex)
+apiHostIO = T.pack <$> catch (envVar "API_HOST") (\(SomeException ex) -> pure $ "API Host Runtime Unset" <> Prelude.show ex)
 
 apiHost ∷ Text
-apiHost = pack $ $(stringE =<< runIO (catch (envVar "API_HOST") (\(SomeException ex) -> pure $ "API Host Compile Time Unset" <> show ex)))
+apiHost = T.pack $ $(stringE =<< runIO (catch (envVar "API_HOST") (\(SomeException ex) -> pure $ "API Host Compile Time Unset" <> Prelude.show ex)))
